e8389ea67289fcfda2cc9ea217407482ef207a4e
Camila Arruda <deco260483@gmail.com>
2026-01-28 14:11:12 -0300

    fix: corrige auto-dismiss do toast com setTimeout

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -110,7 +110,7 @@ function dispatch(action) {
   });
 }
 
-function toast({ ...props }) {
+function toast({ duration = 5000, ...props }) {
   const id = genId();
 
   const update = (props) =>
@@ -133,6 +133,13 @@ function toast({ ...props }) {
       },
     },
   });
+
+  // Auto-dismiss após a duração especificada
+  if (duration > 0) {
+    setTimeout(() => {
+      dismiss();
+    }, duration);
+  }
 
   return {
     id,
